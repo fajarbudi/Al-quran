b305e7a1db682fa98abea3c8a4416e0dac91e6d8
fajarbudi <fajarbudira@gmail.com>
2023-08-06 12:04:36 +0700

    update

--- src/app/component/surat.tsx
+++ src/app/component/surat.tsx
@@ -10,41 +10,41 @@ export default function surat() {
 
     if (isLoading)
       return <h1 className="text-2xl mx-auto">tunggu data.....</h1>;
-      const surah = data.data;
-      return surah.map((surat: any, i: number) => (
-        <Link key={i} href={`/surat/${surat.nomor}`}>
-          <div
-            style={{ height: "190px" }}
-            className="m-6 w-96 md:w-80 rounded-xl text-center claymorpishm  px-2 py-4 grid content-between relative">
-            <div className="flex flex-row justify-between px-4 pt-6">
-              <div className="text-white bg-[#3da9fc] px-8 py-2 rounded-xl claymorpishm1 absolute -top-4">
-                <h1>{surat.namaLatin}</h1>
-              </div>
-              <p>{surat.nomor}.</p>
-              <div className="grid gap-2">
-                <p className="border-b-2">{surat.arti}</p>
-                <div className="flex flex-row justify-center gap-2 pb-2">
-                  <p>{surat.tempatTurun} -</p>
-                  <p>{surat.jumlahAyat} Ayat</p>
-                </div>
-              </div>
-              <p className="text-2xl text-[#094067] flex items-center">
-                {surat.nama}
-              </p>
+    const surah = data.data;
+    return surah.map((surat: any, i: number) => (
+      <Link key={i} href={`/surat/${surat.nomor}`}>
+        <div
+          style={{ height: "190px" }}
+          className="m-6 w-80 rounded-xl text-center claymorpishm  px-2 py-4 grid content-between relative">
+          <div className="flex flex-row justify-between px-4 pt-6">
+            <div className="text-white bg-[#3da9fc] px-8 py-2 rounded-xl claymorpishm1 absolute -top-4">
+              <h1>{surat.namaLatin}</h1>
             </div>
-            <div className=" border-[#3da9fc] border-t-2 pt-2 mx-auto">
-              <audio controls className="rounded-xl bg-[#3da9fc] claymorpishm1">
-                <source src={`${surat.audioFull["05"]}`} />
-              </audio>
+            <p>{surat.nomor}.</p>
+            <div className="grid gap-2">
+              <p className="border-b-2">{surat.arti}</p>
+              <div className="flex flex-row justify-center gap-2 pb-2">
+                <p>{surat.tempatTurun} -</p>
+                <p>{surat.jumlahAyat} Ayat</p>
+              </div>
             </div>
+            <p className="text-2xl text-[#094067] flex items-center">
+              {surat.nama}
+            </p>
           </div>
-        </Link>
-      ));
+          <div className=" border-[#3da9fc] border-t-2 pt-2 mx-auto">
+            <audio controls className="rounded-xl bg-[#3da9fc] claymorpishm1">
+              <source src={`${surat.audioFull["05"]}`} />
+            </audio>
+          </div>
+        </div>
+      </Link>
+    ));
   };
 
   return (
     <>
-      <div className="w-screen">
+      <div>
         <Navbar judul="Al-Quran" position="sticky top-0" />
         <div className="flex justify-center xl:justify-start xl:ml-12  flex-wrap mt-4">
           {hasil()}
