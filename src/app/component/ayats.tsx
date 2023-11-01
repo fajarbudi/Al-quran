79e0367ae6a72fa3a79a7b6929a77de55b5faada
fajarbudi <fajarbudira@gmail.com>
2023-11-01 14:46:47 +0700

    mengubah line height

--- src/app/component/ayats.tsx
+++ src/app/component/ayats.tsx
@@ -61,7 +61,7 @@ export default function ayats(props: any) {
         <div className="flex flex-row justify-between gap-2">
           <p className="text-[#3da9fc] flex-none">( {ayat.nomorAyat}. )</p>
           <p
-            style={{ lineHeight: "250%", wordSpacing: "3px" }}
+            style={{ lineHeight: "220%", wordSpacing: "3px" }}
             className={`text-4xl text-end antialiased text-[#094067] ${myFont.className}`}>
             {`${ayat.teksArab}`}
           </p>
