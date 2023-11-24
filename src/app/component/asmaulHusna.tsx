214986c9078b2e43c3a4c8d61b73f7d5abb2e9dd
fajarbudi <fajarbudira@gmail.com>
2023-11-24 14:18:01 +0700

    update

--- src/app/component/asmaulHusna.tsx
+++ src/app/component/asmaulHusna.tsx
@@ -26,7 +26,7 @@ export default function asmaulHusna() {
   return Data.map((asmaulHusna: type, i: number) => (
     <div
       key={i}
-      className="shadow-claymorpishm1 m-2 p-2 rounded-xl w-48 xl:w-56 xl:m-4 h-32 relative">
+      className="shadow-claymorpishm1 m-2 p-2 rounded-xl w-44 xl:w-56 xl:m-4 h-32 relative">
       <div className="flex flex-row pb-3.5">
         <h1>{i + 1}.</h1>
         <div style={{ marginLeft: "20%" }}>
@@ -37,10 +37,10 @@ export default function asmaulHusna() {
       <div
         id={`${i}`}
         onClick={() => coba(i)}
-        className=" h-10 p-2 shadow-claymorpishm2 rounded-b-xl bg-[#3da9fc] text-white absolute left-0 bottom-0 w-48 xl:w-56 cursor-pointer overflow-hidden">
+        className=" h-10 p-2 shadow-claymorpishm2 rounded-b-xl bg-[#3da9fc] text-white absolute left-0 bottom-0 w-44 xl:w-56 cursor-pointer overflow-hidden">
         <p id={`preview${i}`} className="text-center ">
-          {asmaulHusna.arti.length > 18
-            ? `${asmaulHusna.arti.substring(0, 18)}...`
+          {asmaulHusna.arti.length > 16
+            ? `${asmaulHusna.arti.substring(0, 16)}...`
             : asmaulHusna.arti}
         </p>
         <p id={`full${i}`} className="text-center hidden">
